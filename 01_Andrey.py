59aa4bfa511b2a173129a227553bce7d0edecf83
André Luis Marques Ferreira dos Santos <andrelmfsantos@gmail.com>
2020-03-26 01:05:46 -0300

    Add files via upload

---
+++ 01_Andrey.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Meu primeiro script
+
+# In[1]:
+
+
+4 * 3   # operação de multiplicação
+
+
+# In[2]:
+
+
+7 / 3 # operação de divisão
+
+
+# In[3]:
+
+
+7 % 4 # resto da divisão
+
+
+# In[9]:
+
+
+4 ** 2 # potenciação
+
+
+# In[5]:
+
+
+7 // 3 # operação de divisão e dá como resposta apenas o valor inteiro (desconsidera o decimal)
+
+
+# In[6]:
+
+
+4 - 3 # subtração
+
+
+# In[13]:
+
+
+16**(1/2) # radiciação
+
